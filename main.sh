39d2f233c6344b665f99b47a49d74d321853fc48
Giacomo Ziveri <102797292+gzifalsi@users.noreply.github.com>
2024-05-03 00:59:59 +0200

    fixed an error

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 #disable usb autosuspension
 
 cp /etc/default/grub /etc/default/grub.backup #backup /etc/default/grub
-params=$(cat grubtemp | grep GRUB_CMDLINE_LINUX_DEFAULT) #keep the current kernel parameters
+params=$(cat /etc/default/grub | grep GRUB_CMDLINE_LINUX_DEFAULT) #keep the current kernel parameters
 params=$(echo "$params" | sed 's/.$//')
 params="$params usbcore.autosuspend=-1\"" #add usbcore.autosuspend=-1 to the kernel parameters
 sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/s/.*/$params/" /etc/default/grub #apply changes
